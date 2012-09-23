effe81e18de338c6b11083c3befdc46818cd71b1
Marco Garcia <marcogaroz@gmail.com>
2012-09-23 18:38:30 -0500

    add initial tarea y raiz

--- app/models/client.rb
+++
@@ -1,4 +0,0 @@
-class Client < ActiveRecord::Base
-  #attr_accessible :card_number, :lastname, :name
-  has_many :products
-end
--- app/models/product.rb
+++
@@ -1,4 +0,0 @@
-class Product < ActiveRecord::Base
-  #attr_accessible :client, :description, :id_product, :price, :references
-  belong_to :client
-end
